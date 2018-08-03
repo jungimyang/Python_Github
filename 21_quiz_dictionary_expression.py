a042c46ea70050f94413dcddd1a17056dbbbf433
igotit <wjddla1102@nate.com>
2018-08-03 10:43:39 +0900

    딕셔너리 표현식 퀴즈

---
+++ 21_quiz_dictionary_expression.py
@@ -0,0 +1,12 @@
+'''표준 입력으로 문자열 여러 개와 숫자 여러 개가 두 줄로 입력되고,
+첫 번째 줄은 키, 두 번째 줄은 값으로 하여 딕셔너리를 생성합니다.
+딕셔너리에서 키가 'delta'인 키-값 쌍과 값이 30인 키-값 쌍을 삭제하도록 만드세요.
+'''
+keys = input().split()
+values = map(int,input().split())
+
+x = dict(zip(keys,values))
+x.pop('delta') #  키-값 쌍을 삭제
+x = {key:value for key,value in x.items() if value != 30} #딕셔너리 표현식을 사용하여 값이 30인 키-값 쌍만 제외
+
+print(x)
